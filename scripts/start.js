b95376bf0beb38af359b4d0ac9165b7ade25de4e
dahiryusuf <dahir.yusuf@hotmail.com>
2022-03-21 02:00:24 -0400

    fix images

--- scripts/start.js
+++ scripts/start.js
@@ -4,10 +4,9 @@ const upath = require('upath');
 const browserSyncPath = upath.resolve(upath.dirname(__filename), '../node_modules/.bin/browser-sync');
 
 concurrently([
-    { command: 'node scripts/sb-watch.js', name: 'SB_WATCH', prefixColor: 'bgBlue.bold' },
-    { 
+    {
         command: `"${browserSyncPath}" --reload-delay 2000 --reload-debounce 2000 dist -w --no-online`,
-        name: 'SB_BROWSER_SYNC', 
+        name: 'SB_BROWSER_SYNC',
         prefixColor: 'bgGreen.bold',
     }
 ], {
@@ -16,7 +15,7 @@ concurrently([
 }).then(success, failure);
 
 function success() {
-    console.log('Success');    
+    console.log('Success');
 }
 
 function failure() {
